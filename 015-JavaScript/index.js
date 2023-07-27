890303fa2a6076a7091668d42b93e54435e2a507
Agatha Teixeira <agathapamela2020@gmail.com>
2023-07-27 14:01:32 -0300

    Revisão do exercício para estudos - exercício refeito

--- 015-JavaScript/index.js
+++ 015-JavaScript/index.js
@@ -1,89 +1,88 @@
-let option = parseInt(
-  prompt(
-    "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-  )
-);
-do {
-  switch (option) {
-    case 1:
-      function calcularAreaTriangulo() {
-        const base = parseInt(prompt("Insira o tamanho da base"));
-        const altura = parseInt(prompt("Insira o tamanho da altura"));
+//funcao para exibir menu
+let menu = function () {
+  return parseInt(
+    prompt(
+      "Insira um número de 1 a 5 para calcular a área e" +
+        "\n6 para sair do programa:" +
+        "\n1 - Triângulo" +
+        "\n2 - Retângulo" +
+        "\n3 - Quadrado" +
+        "\n4 - Trapézio" +
+        "\n5 - Círculo" +
+        "\n6 - Sair"
+    )
+  );
+};
 
-        return (base * altura) / 2;
-      }
-      alert("Área do triângulo = " + calcularAreaTriangulo());
-      option = parseInt(
-        prompt(
-          "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-        )
-      );
-      break;
-    case 2:
-      function calcularAreaRetangulo() {
-        const base = parseInt(prompt("Insira o tamanho da base"));
-        const altura = parseInt(prompt("Insira o tamanho da altura"));
+//function p calcular area triangulo
+let areaTriangulo = function () {
+  const base = parseInt(prompt("Insira o valor da base"));
+  const altura = parseInt(prompt("Insira o valor da altura"));
+  const area = (base * altura) / 2;
+  return area;
+};
 
-        return base * altura;
-      }
-      alert("Área do retângulo = " + calcularAreaRetangulo());
-      option = parseInt(
-        prompt(
-          "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-        )
-      );
-      break;
-    case 3:
-      function calcularAreaQuadrado() {
-        const lado = parseInt(prompt("Insira o tamanho do lado"));
+//function p calcular area retangulo
+let areaRetangulo = function () {
+  const base = parseInt(prompt("Insira o valor da base"));
+  const altura = parseInt(prompt("Insira o valor da altura"));
+  const area = base * altura;
+  return area;
+};
 
-        return lado * lado;
-      }
-      alert("Área do quadrado = " + calcularAreaQuadrado());
-      option = parseInt(
-        prompt(
-          "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-        )
-      );
-      break;
-    case 4:
-      function calcularAreaTrapezio() {
-        const baseMaior = parseInt(prompt("Insira o tamanho da base maior"));
-        const baseMenor = parseInt(prompt("Insira o tamanho da base menor"));
-        const altura = parseInt(prompt("Insira o tamanho da altura"));
+//function p calcular area quadrado
+let areaQuadrado = function () {
+  const lado = parseInt(prompt("Insira o valor do lado"));
+  const area = lado * lado;
+  return area;
+};
 
-        return ((baseMaior + baseMenor) * altura) / 2;
-      }
-      alert("Área do trapézio = " + calcularAreaTrapezio());
-      option = parseInt(
-        prompt(
-          "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-        )
-      );
-      break;
-    case 5:
-      function calcularAreaCirculo() {
-        const raio = parseInt(prompt("Insira o tamanho do raio"));
-        const pi = 3.14;
+//function p calcular area trapezio
+let areaTrapezio = function () {
+  const baseMaior = parseInt(prompt("Insira o valor da base maior"));
+  const baseMenor = parseInt(prompt("Insira o valor da base menor"));
+  const altura = parseInt(prompt("Insira o valor da altura"));
+  const area = ((baseMaior + baseMenor) * altura) / 2;
+  return area;
+};
 
-        return pi * (raio * raio);
-      }
-      alert("Área do Círculo = " + calcularAreaCirculo());
-      option = parseInt(
-        prompt(
-          "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-        )
-      );
-      break;
-    case 6:
-      alert("Saindo...");
-      break;
-    default:
-      alert("Insira uma opção válida!");
-      option = parseInt(
-        prompt(
-          "Insira o número correspondente para calcular a área: \n1 - Triângulo\n2 - Retânguolo\n3 - Quadrado\n4 - Trapézio\n5 - Círculo\n6 - Sair"
-        )
-      );
-  }
-} while (option !== 6);
+//function p calcular area do circulo
+let areaCirculo = function () {
+  const pi = 3.14;
+  const raio = parseInt(prompt("Insira o valor do raio"));
+  const area = pi * (raio * raio);
+  return area;
+};
+
+//function para rodar o programa
+const programa = function () {
+  let option = "";
+  do {
+    option = menu();
+    switch (option) {
+      case 1:
+        alert("Área do Triângulo = " + areaTriangulo());
+        break;
+      case 2:
+        alert("Área do Retângulo = " + areaRetangulo());
+        break;
+      case 3:
+        alert("Área do Quadrado = " + areaQuadrado());
+        break;
+      case 4:
+        alert("Área do Trapezio = " + areaTrapezio());
+        break;
+      case 5:
+        alert("Área do Círculo = " + areaCirculo());
+        break;
+      case 6:
+        alert("Obrigado por usar a calculadora de áreas!" + "\nSaindo...");
+        break;
+      default:
+        alert("Insira uma opção válida!");
+        break;
+    }
+  } while (option !== 6);
+};
+
+programa();
